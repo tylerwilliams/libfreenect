cd88596f52c0daeedfe5d2045c7990f8aaf4cfba
Tyler Williams <williams.tyler@gmail.com>
2010-11-17 15:43:46 -0500

    remove c, replace with libfreenect

---
+++ libfreenect/cameras.h
@@ -0,0 +1,31 @@
+/*  libfreenect - an open source Kinect driver
+
+Copyright (C) 2010  Hector Martin "marcan" <hector@marcansoft.com>
+
+This code is licensed to you under the terms of the GNU GPL, version 2 or version 3;
+see:
+ http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+ http://www.gnu.org/licenses/gpl-3.0.txt
+*/
+
+#ifndef CAMERAS_H
+#define CAMERAS_H
+
+
+#include <libusb.h>
+
+struct caminit {
+	uint16_t command;
+	uint16_t tag;
+	int cmdlen;
+	int replylen;
+	uint8_t cmddata[1024];
+	uint8_t replydata[1024];
+};
+
+typedef void (*depthcb)(uint16_t *buf, int width, int height);
+typedef void (*rgbcb)(uint8_t *buf, int width, int height);
+
+void cams_init(libusb_device_handle *d, depthcb depth_cb, rgbcb rgb_cb);
+
+#endif

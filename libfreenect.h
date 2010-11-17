81667cc7444d8ecf96162cab90f6a65be504709e
Tyler Williams <williams.tyler@gmail.com>
2010-11-17 15:47:22 -0500

    add autotools, examples

---
+++ libfreenect.h
@@ -0,0 +1,16 @@
+/*  libfreenect - an open source Kinect driver
+
+Copyright (C) 2010  Hector Martin "marcan" <hector@marcansoft.com>
+
+This code is licensed to you under the terms of the GNU GPL, version 2 or version 3;
+see:
+ http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+ http://www.gnu.org/licenses/gpl-3.0.txt
+*/
+
+#ifndef LIBFREENECT_H
+#define LIBFREENECT_H
+
+#include <libfreenect/cameras.h>
+
+#endif //LIBFREENECT_H
